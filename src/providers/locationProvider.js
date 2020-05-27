b5a6318cd6ca5eecd47c485a0cb15216e90212f2
Estefan Hu <estefanhu074@gmail.com>
2020-05-27 11:50:38 -0700

    added location provider

---
+++ src/providers/locationProvider.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export const LocationContext = React.createContext({
+  longitude: null,
+  latitude: null,
+  setLongitude: () => {},
+  setLatitude: () => {}
+});
+
+export const LocationProvider = ({ children }) => {
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+
+  return (
+    <LocationContext.Provider value={{
+      longitude,
+      latitude,
+      updateLocation: () => {
+        navigator.geolocation.getCurrentPosition(position => {
+          setLongitude(position.coords.longitude);
+          setLatitude(position.coords.latitude);
+        }, error => console.log(error));
+      }
+    }}>
+      {children}
+    </LocationContext.Provider>
+  )
+}
